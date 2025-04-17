8d4cf3aba24339c93bf91eadf28faad959b6350a
John Paul Gabule <lucas.gabule@gmail.com>
2025-04-18 00:32:08 +0800

    dsa: recursive find solution for a number

--- recursion/power.js
+++ recursion/power.js
@@ -6,6 +6,22 @@ function power(base, exponent) {
   }
 }
 
-console.log(power(2, 3));
+// console.log(power(2, 3));
 
+function findSolution(target) {
+  function find(current, history) {
+    if(current == target) {
+      return history;
+    } else if(current > target) {
+      return null;
+    } else {
+      return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
+    }
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24)); // (((1 * 3) + 5) * 3)
+console.log(findSolution(34)); // (((((1 * 3) + 5) * 3) + 5) + 5)
+console.log(findSolution(36)); // (((((((1 + 5) + 5) + 5) + 5) + 5) + 5) + 5)
 
